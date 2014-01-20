fe0f1e2f314c29dd5a8bdda24c55a1020b86c6f6
Alex Bahouth <abahouth@onekingslane.com>
2014-01-20 14:42:02 -0800

    Handle exceptions with DB queries. Still working on getting SQLlite to be an inmemory database

--- src/dwd/core.clj
+++ src/dwd/core.clj
@@ -3,12 +3,10 @@
 back whether they return 1 or 0"
   {:author "Alex Bahouth"
    :date "Jan 20, 2014"}
-  (:require
-   [clojure.string :refer [join]]
-   [clojure.java.jdbc :as j])
-  (:require
-   [diesel.core :refer :all]
-   [roxxi.utils.print :refer :all]))
+  (:require [clojure.string :refer [join]]
+            [clojure.java.jdbc :as j])
+  (:require [diesel.core :refer :all]
+            [roxxi.utils.print :refer :all]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; helpers
@@ -33,6 +31,9 @@ back whether they return 1 or 0"
 ;; TODO make better
 (def results (atom []))
 
+(defn- add-result! [val]
+  (swap! results #(conj % val)))
+
 (definterpreter exec-interp [env]
   ['testing => :testing]
   ['check => :check]
@@ -41,10 +42,11 @@ back whether they return 1 or 0"
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ## testing
-;; ex. `'(testing DESC EXPR)`
+;; ex. `'(testing DESC EXPR+)`
 ;;
-(defmethod exec-interp :testing [[_ desc expr] env]
-  (exec-interp expr (append-desc env desc)))
+(defmethod exec-interp :testing [[_ desc & exprs] env]
+  (let [new-env (append-desc env desc)]
+    (map #(exec-interp % new-env) exprs)))
 
 
 
@@ -54,14 +56,40 @@ back whether they return 1 or 0"
   (CheckResult. id desc val))
 
 
+
+(defn- result->pass-fail
+  "It's assumed that an vec of vecs is returned
+where the first vec contains the vec of column headers (of
+which there should only be one) and the second vec is an
+vec of values of which there should only be one"
+  [result]
+  (let [extracted-value (first (second (print-expr result)))]
+    (cond
+     (= extracted-value 1) :pass
+     (= extracted-value 0) :fail)
+    :else
+    (do
+      ;; log this
+      (print-expr (str "Unable to determine whether >>>"
+                       result
+                       "<<< is a success or failure"))
+      :error)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ## check
 ;; ex. `'(check DESC EXPR)`
 ;;
-(defmethod exec-interp :check [[_ id desc expr] env]
+(defmethod exec-interp :check [[_ desc expr] env]
   (let [new-env (append-desc env desc)
-        check-result (exec-interp expr new-env)]
-    (swap! results #(conj % (make-check-result id (:desc new-env) check-result)))))
+        result (try
+                 (exec-interp expr new-env)
+                 (catch java.lang.Exception e
+                     (print-expr e)))
+        pass-fail (result->pass-fail result)
+        check-result (make-check-result (or (:id new-env) :anon)
+                                        (:desc new-env)
+                                        pass-fail)]
+    (add-result! check-result )))
 
 
 (defmethod exec-interp :with-db [[_ conn-info-or-sym expr] env]
@@ -70,6 +98,9 @@ back whether they return 1 or 0"
                     conn-info-or-sym)
         new-env (config-db env conn-info)]
     (exec-interp expr new-env)))
+
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ## query
@@ -81,7 +112,7 @@ back whether they return 1 or 0"
 ;; ```
 ;;
 (defmethod exec-interp :query [[_ sql-query] env]
-  (print-expr (j/query (:db-conn-info env) [sql-query] :as-arrays? true)))
+  (j/query (:db-conn-info env) [sql-query] :as-arrays? true))
 
 
 
@@ -96,25 +127,36 @@ back whether they return 1 or 0"
   (let [e #(j/execute! sql-lite-test-db %)
         q #(j/query sql-lite-test-db %)]
     (e ["CREATE TABLE IF NOT EXISTS test (name varchar(10), age int)"])
-    (e ["INSERT INTO test SELECT 'Alex' AS name, 30 AS age
-UNION SELECT 'Karl', 28
-UNION SELECT 'Eric', 29
-UNION SELECT 'Egor', 23"])))
+    (e [(join " "
+              ["INSERT INTO test SELECT 'Alex' AS name, 30 AS age"
+               "UNION SELECT 'Karl', 28"
+               "UNION SELECT 'Eric', 29"
+               "UNION SELECT 'Egor', 23"])])))
+
 
 (bootstrap-sqlite)
 
 
 (deforder three-test
   (testing "if it's truth that"
-    (check :my-id "this returns 5"
+    (check "this returns 5"
            (with-db sql-lite-test-db
              (query "select CASE WHEN count(*) > 3 THEN 1 ELSE 0 END AS a_test from test")))))
 
 (deforder four-test
-  (testing "can we check"
+  (with-db sql-lite-test-db
     (testing "if it's truth that"
-      (check :my-id "this returns 5"
-             (query 5)))))
+      (check "this returns 5"
+             (query "select CASE WHEN count(*) > 3 THEN 1 ELSE 0 END AS a_test from test"))
+      (check "this returns 10"
+             (query "select CASE WHEN count(*) > 20 THEN 1 ELSE 0 END AS a_test from test")))))
+
+(deforder five-test
+  (testing "can we check"
+    (with-db sql-lite-test-db
+      (testing "if it's truth that"
+        (check "this returns 5"
+               (query "select 5 from test limit 1"))))))
 
 
 
@@ -123,6 +165,8 @@ UNION SELECT 'Egor', 23"])))
 ;; (exec-interp '(query 5) {})
 
 (print-expr (exec-interp three-test {}))
+(print-expr (exec-interp four-test {}))
+(print-expr (exec-interp five-test {}))
 (print-expr @results)
 ;;(print-expr (exec-interp four-test {}))
 (swap! results empty)
