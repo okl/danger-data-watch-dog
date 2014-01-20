fe22ef82ad02245ded540c3b18e19131a3576c64
Alex Bahouth <abahouth@onekingslane.com>
2014-01-20 09:11:19 -0800

    Clean new project

---
+++ src/dwd/core.clj
@@ -0,0 +1,6 @@
+(ns dwd.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/dwd/core_test.clj
@@ -0,0 +1,7 @@
+(ns dwd.core-test
+  (:use clojure.test
+        dwd.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
