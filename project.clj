bd44343438471e778e975c8f8059467b374dc746
Alex Bahouth <abahouth@onekingslane.com>
2014-01-20 12:51:18 -0800

    checkpoint

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/java.jdbc "0.3.2"]
+                 [com.onekingslane.danger/diesel "1.0.1"]
+                 [com.onekingslane.danger/clojure-common-utils "0.0.19"]]
+  :profiles {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.7.2"]]}})
--- src/dwd/core.clj
+++ src/dwd/core.clj
@@ -1,6 +1,98 @@
-(ns dwd.core)
+(ns dwd.core
+  "Creating a small DSL to run queries and report
+back whether they return 1 or 0"
+  {:author "Alex Bahouth"
+   :date "Jan 20, 2014"}
+  (:require
+   [clojure.string :refer [join]])
+  (:require
+   [diesel.core :refer :all]
+   [roxxi.utils.print :refer :all]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; helpers
+(defn- joins
+  "Join with spaces"
+  [& strs]
+  (join " " strs))
+
+(defn- append-desc [env-map desc]
+  (update-in env-map [:desc] #(joins % desc)))
+
+(defn- config-db [env-map conn-info]
+  (update-in env-map [:db-conn-info] #(joins % conn-info)))
+
+
+
+(defmacro deforder [id orders]
+  `(def ~id (quote ~orders)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; # The language
+
+;; TODO make better
+(def results (atom []))
+
+(definterpreter exec-interp [env]
+  ['testing => :testing]
+  ['check => :check]
+  ['query => :query]
+  ['with-db => :with-db])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ## testing
+;; ex. `'(testing DESC EXPR)`
+;;
+(defmethod exec-interp :testing [[_ desc expr] env]
+  (exec-interp expr (append-desc env desc)))
+
+
+(defn- make-rec [id desc val]
+  {:id id :desc desc :result val})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ## check
+;; ex. `'(check DESC EXPR)`
+;;
+(defmethod exec-interp :check [[_ id desc expr] env]
+  (let [new-env (append-desc env desc)
+        check-result (exec-interp expr new-env)]
+    (swap! results #(conj % (make-rec id (:desc new-env) check-result)))))
+
+
+;; (defmethod exec-interp :with-db [[_ conn-info expr] env]
+;;   (binding
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ## query
+;; ex.
+;; ```
+;; (with-db DB-CONN
+;;   ...
+;;   '(query QUERY)+)
+;; ```
+;;
+(defmethod exec-interp :query [[_ sql-query] env] sql-query)
+
+
+
+(deforder three-test
+  (testing "if it's truth that"
+    (check :my-id "this returns 5"
+           (query 5))))
+
+(deforder four-test
+  (testing "can we check"
+    (testing "if it's truth that"
+      (check :my-id "this returns 5"
+             (query 5)))))
+
+
+
+
+
+(exec-interp '(query 5) {})
+(print-expr @results)
+(print-expr (exec-interp three-test {}))
+(print-expr (exec-interp four-test {}))
+(swap! results empty)
