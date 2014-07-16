6319d4a6799f612edcd111fe15144b20732935e7
Eric Sayle <esayle@onekingslane.com>
2014-07-16 16:34:08 -0700

    Adding interpreter to associate ids with things

---
+++ src/dwd/id_interp.clj
@@ -0,0 +1,88 @@
+(ns dwd.id-interp
+  "Process the check config to get one per end result, with ids"
+  {:author "Eric Sayle"
+   :date "Wed Jul 16 11:25:06 PDT 2014"}
+  (:require [diesel.core :refer :all])
+  )
+
+;; I only need to interpert what could be grouping checks together
+(definterpreter id-interp [env]
+  ['testing => :testing]
+  ['check => :check]
+;  ['lookup-config => :lookup-config]
+  ['define-config => :define-config]
+  ;; Supporting Databases
+  ['with-db => :with-db]
+;  ['query => :query]
+  ;; Supporting Files
+  ['with-s3 => :with-s3]
+  ['with-ftp => :with-ftp]
+  ;; Predicates
+;  ['= => :=]
+;  ['>= => :>=]
+;  ['file-present? => :file-present?]
+  ;; Grouping
+  ['group => :group])
+
+(defn- apply-function-to-map-values [m f]
+  (into {} (for [[k v] m] [k (f v)])))
+
+(defn- add-list [inmap args]
+  "Takes a map, and wraps each key in a list that starts with args and ends
+   with the key value"
+  (apply-function-to-map-values inmap #(apply list (concat args [%]))))
+
+(defn- create-sublist [env exprs & args]
+  (let [arg-configs (map #(id-interp % env) exprs)
+        updated-configs (map #(add-list % args) arg-configs)]
+    (apply merge updated-configs)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; testing
+;; blah blah what it says everywhere else
+(defmethod id-interp :testing [[_ desc & args ] env]
+  (create-sublist env args 'testing desc))
+
+(defn- process-check-args [args]
+  (if (= (count args) 3)
+    args
+    (cons (gensym "check_") args)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; check
+;; blah blah
+(defmethod id-interp :check [[_ & args] env]
+  (let [[id desc expr] (process-check-args args)]
+    {id (list 'check id desc expr)}))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; define-config
+(defmethod id-interp :define-config [[_ id config-map & exprs] env]
+  (create-sublist env exprs 'define-config id config-map))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; with-db
+(defmethod id-interp :with-db [[_ conn-info-or-sym & exprs] env]
+  (create-sublist env exprs 'with-db conn-info-or-sym))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; with-s3
+(defmethod id-interp :with-s3 [[_ s3-config & exprs] env]
+  (create-sublist env exprs 'with-s3 s3-config))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; with-ftp
+(defmethod id-interp :with-ftp [[_ ftp-config & exprs] env]
+  (create-sublist env exprs 'with-ftp ftp-config))
+
+(defn- process-group-args [args]
+  (if (= (count args) 3)
+    args
+    (cons (gensym "group_") args)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; group
+(defmethod id-interp :group [[_ & args] env]
+  (let [[id desc checks] (process-group-args args)
+        check-defs (filter seq? checks)
+        group-def {id (list 'group id desc checks)}]
+    (merge group-def (apply merge (map #(id-interp % {}) check-defs)))))
---
+++ test/dwd/id_interp_test.clj
@@ -0,0 +1,88 @@
+(ns dwd.id-interp-test
+  (:require [clojure.test :refer :all])
+  (:require [dwd.id-interp :refer :all]))
+
+(defn check-explicit-id [id]
+  (list 'check id "testing" '(= 1 1)))
+
+(def check-anon-id
+  '(check "testing" (= 1 1)))
+
+
+(deftest test-check
+  (testing "explicit ids"
+    (is (= (id-interp (check-explicit-id 'id) {})
+           {'id (check-explicit-id 'id)})))
+  (testing "anonymous ids"
+    (let [interp-output (id-interp check-anon-id {})
+          key (first (keys interp-output))]
+      (is (= interp-output
+             {key (check-explicit-id key)})))))
+
+(deftest test-testing
+  (testing "single ids"
+    (testing "explicit id"
+      (is (= (id-interp
+              (list 'testing "something" (check-explicit-id 'my-id))
+              {})
+             {'my-id (list
+                      'testing
+                      "something"
+                      (check-explicit-id 'my-id))})))
+    (testing "anonymous id"
+      (let [expr (list 'testing "something" check-anon-id)
+            interp-output (id-interp expr {})
+            key (first (keys interp-output))]
+        (is (= interp-output
+               {key (list 'testing "something" (check-explicit-id key))})))))
+  (testing "multiple ids"
+    (let [expr (list 'testing
+                     "something"
+                     check-anon-id
+                     (check-explicit-id 'id))
+          interp-output (id-interp expr {})
+          result-keys (keys interp-output)
+          named-result (list 'testing "something" (check-explicit-id 'id))
+          anon-result (list 'testing "something" (check-explicit-id (second result-keys)))]
+      (is (= interp-output
+             {'id named-result
+              (second result-keys) anon-result}))))
+  (testing "multiple levels"
+    (let [expr (list 'testing
+                     "something"
+                     check-anon-id
+                     (list 'testing "something" (check-explicit-id 'id)))
+          interp-output (id-interp expr {})
+          result-keys (keys interp-output)
+          named-result (list 'testing
+                             "something"
+                             (list 'testing "something" (check-explicit-id 'id)))
+          anon-result (list 'testing
+                            "something"
+                            (check-explicit-id (second result-keys)))]
+      (is (= interp-output
+             {'id named-result
+              (second result-keys) anon-result})))))
+
+(deftest test-groups
+  (testing "id-group-only-ids"
+    (is (= (id-interp '(group id "mygroup" [someid]) {})
+            {'id '(group id "mygroup" [someid])})))
+  (testing "id-group-check-def"
+    (is (= (id-interp '(group groupid "mygroup" [(check checkid "mycheck" (= 1 1))]) {})
+           {'groupid '(group groupid "mygroup" [(check checkid "mycheck" (= 1 1))])
+            'checkid '(check checkid "mycheck" (= 1 1))})))
+  (testing "id-group-check-def-and-id"
+    (let [check-expr '(check checkdef "mycheck" (= 1 1))
+          group-expr (list 'group 'groupid "mygroup" [check-expr 'othercheck])]
+      (is (= (id-interp group-expr {})
+             {'groupid group-expr
+              'checkdef check-expr})))))
+
+(deftest test-mixed
+  (let [check-expr '(check checkdef "mycheck" (= 1 1))
+        group-expr (list 'group 'groupdef "mygruop" ['something check-expr 'else])
+        testing-expr (list 'testing "somestring" group-expr)]
+    (is (= (id-interp testing-expr {})
+        {'groupdef testing-expr
+         'checkdef '(testing "somestring" (check checkdef "mycheck" (= 1 1)))}))))
