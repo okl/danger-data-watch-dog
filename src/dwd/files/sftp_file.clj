bd98c81171f5319cb986a801d9e08bbcd09bf19f
Eric Sayle <esayle@onekingslane.com>
2014-07-24 18:14:04 -0700

    Adding file size and mtime for sftp

--- src/dwd/files/sftp_file.clj
+++ src/dwd/files/sftp_file.clj
@@ -7,20 +7,66 @@
   (:require [dwd.files.file :refer [File]]
             [dwd.check-result :refer [make-check-result]]))
 
+(defprotocol SftpConnection
+  (list-file [_ file])
+  (stat-file [_ file]))
+
+(defn- exec-sftp-command [hostname port username password command file]
+  (try
+    (sftp hostname command file
+          :username username
+          :password password
+          :port port)
+    (catch Exception e
+      e)))
+
+(defn- get-sftp-connection [hostname port username password]
+  (reify SftpConnection
+    (list-file [_ file]
+      (exec-sftp-command hostname port username password :ls file))
+    (stat-file [_ file]
+      (exec-sftp-command hostname port username password :stat file))))
+
 (deftype SftpFile [username password hostname port file desc]
   File
   (file-present? [_]
-    (let [file-names (try
-                       (sftp hostname :ls file
-                             :username username
-                             :password password
-                             :port port)
-                       (catch Exception e
-                         e))
+    (let [file-names (list-file
+                      (get-sftp-connection hostname port username password)
+                      file)
           result (and (not (instance? Exception file-names))
                       (= (count file-names) 1))
           exceptions (when (instance? Exception file-names) file-names)
           messages file-names]
+      (make-check-result
+       {:result result
+        :data file
+        :desc desc
+        :exceptions exceptions
+        :messages messages})))
+  (file-mtime [_]
+    (let [file-stat (stat-file
+                     (get-sftp-connection hostname port username password)
+                     file)
+          result (if (instance? Exception file-stat)
+                   :error
+                   (.getMTime file-stat))
+          exceptions (when (instance? Exception file-stat) file-stat)
+          messages file-stat]
+      (make-check-result
+       {:result result
+        :data file
+        :desc desc
+        :exceptions exceptions
+        :messages messages})))
+  (file-size [_]
+    (let [file-stat (stat-file
+                     (get-sftp-connection hostname port username password)
+                     file)
+          result (if (instance? Exception file-stat)
+                   :error
+                   (.getSize file-stat))
+          exceptions (when (instance? Exception file-stat) file-stat)
+          messages file-stat]
       (make-check-result
        {:result result
         :data file
