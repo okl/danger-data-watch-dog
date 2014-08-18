6760f49e104174cee878e0f4617d25869770275f
Matt Halverson <mhalverson@onekingslane.com>
2014-08-18 10:20:21 -0700

    Add docstring for FileSystem

--- src/dwd/files/file_system.clj
+++ src/dwd/files/file_system.clj
@@ -4,4 +4,5 @@
    :date "Fri Aug 15 2014"})
 
 (defprotocol FileSystem
-  (list-files-matching-prefix [_ prefix]))
+  (list-files-matching-prefix [_ prefix]
+    "Lists all files matching the specified prefix-string, s3-key style."))
